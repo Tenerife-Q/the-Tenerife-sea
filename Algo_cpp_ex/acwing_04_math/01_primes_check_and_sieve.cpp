066add69b5f7a64cd2b787fd7f0b1dec158e75ef
Tenerife <2892015852@qq.com>
2026-03-06 21:41:02 +0800

    Update 01_primes_check_and_sieve.cpp

--- Algo_cpp_ex/acwing_04_math/01_primes_check_and_sieve.cpp
+++ Algo_cpp_ex/acwing_04_math/01_primes_check_and_sieve.cpp
@@ -100,7 +100,16 @@ void get_primes_euler(int n) {
     }
 }
 
-
+void get_primes_euler(int n) {
+    cnt = 0;
+    for(int i = 2; i <= n; i++) {
+        if(!st[i]) primes[cnt++] = i;
+        for(int j = 0; primes[j] * i <= n; j++ ) {
+            st[primes[j] * i] = true;
+            if(i % primes[j] == 0) break;
+        } 
+    }
+}
 
 int main() {
     // 基础功能测试
