8af9cd61873d8b526bf68eff53ff8d588a3dcb42
Tenerife <2892015852@qq.com>
2026-03-09 20:50:04 +0800

    Update 04_combinatorics_and_euler.cpp

--- Algo_cpp_ex/acwing_04_math/04_combinatorics_and_euler.cpp
+++ Algo_cpp_ex/acwing_04_math/04_combinatorics_and_euler.cpp
@@ -136,7 +136,23 @@ void get_primes_euler(int n) {
 }
 */
 
-
+void get_eulers(int n) {
+    euler[1] = 1;
+    for(int i = 2; i <= n; i++) {
+        if(!st[i]) {
+            primes[cnt++] = i;
+            euler[i] = i - 1;
+        }
+        for(int j = 0; primes[j] <= n / i; j++) {
+            st[primes[j] * i] = true;
+            if(i % primes[j] == 0) {
+                euler[primes[j] * i] = euler[i] * primes[j];
+                break;
+            }
+            euler[primes[j] * i] = euler[i] * (primes[j] - 1);
+        }
+    }
+}
 
 
 
