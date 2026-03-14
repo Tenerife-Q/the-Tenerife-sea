b0c12736b2b28fb6cca59e8595dd20a0d845384a
Tenerife <2892015852@qq.com>
2026-03-14 17:15:49 +0800

    Append recursive exponentiation example
    Add a commented recursive implementation of fast (binary) exponentiation with a sample main that uses modulus 1e9+7. This provides an alternative to the existing iterative qmi function and includes an explanatory comment about modular squaring. Also add a missing newline at end of file.

--- Algo_cpp_ex/CSES/Mathematics/02_Exponentiation.cpp
+++ Algo_cpp_ex/CSES/Mathematics/02_Exponentiation.cpp
@@ -87,3 +87,29 @@ int main() {
         cout << qmi(a, k, p) << "\n";
     }
 }
+
+
+
+// recursive version
+// 这里可以详见应用密码学封面模平方计算从左往右 上面那个算法更简单易懂 是从左往右
+// #include <iostream>
+// using namespace std;
+// using ll = long long;
+
+// int power(int a, int b, int m) {
+//     if (b == 0) return 1 % m;
+//     ll p = power(a, b / 2, m);
+//     p = p * p % m;
+//     if (b % 2 == 1) p = p * a % m;
+//     return p;
+// }
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++) {
+//         int a, b;
+//         cin >> a >> b;
+//         cout << power(a, b, 1e9 + 7) << "\n";
+//     }
+// }
