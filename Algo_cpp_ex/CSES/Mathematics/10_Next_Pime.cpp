0e372ef17e8297ee3f7f365363d17a36bd0ff478
Tenerife <2892015852@qq.com>
2026-03-18 21:47:34 +0800

    Update 10_Next_Pime.cpp

--- Algo_cpp_ex/CSES/Mathematics/10_Next_Pime.cpp
+++ Algo_cpp_ex/CSES/Mathematics/10_Next_Pime.cpp
@@ -142,3 +142,35 @@ int main() {
     
     return 0;
 }
+
+
+
+bool is_prime(ll n) {
+    if(n <= 1) return false;
+    if(n <= 3) return true;
+    if(n % 2 == 0 || n % 3 == 0) return false;
+    for (ll i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll m;
+        cin >> m;
+        m++;
+        while(!is_prime(m)) {
+            m++;
+        }
+        cout << m << "\n";
+    }
+    return 0;
+}
