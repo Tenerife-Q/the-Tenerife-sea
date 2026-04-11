9108aef6f7fd36b8a933f0eca0548adc1177a076
Tenerife <2892015852@qq.com>
2026-04-11 15:20:14 +0800

    Update 37_Another_Game.cpp

--- Algo_cpp_ex/CSES/Mathematics/37_Another_Game.cpp
+++ Algo_cpp_ex/CSES/Mathematics/37_Another_Game.cpp
@@ -61,3 +61,25 @@ int main() {
     }
     return 0;
 }
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int ti = 1; ti <= t; ti++) {
+        int n;
+        cin >> n;
+        bool win = false;
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            if (x % 2 == 1) win = true;
+        }
+        cout << (win ? "first" : "second") << "\n";
+    }
+}
